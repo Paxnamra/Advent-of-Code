872e7516fd82a728393668f7f319572bc39eca33
Paxnamra <bialyfeniks4@gmail.com>
2019-12-29 16:14:49 +0100

    IntUnaryOperator and IntBiaryOperator functions fun

--- src/main/java/day1/task1/Execute.java
+++ src/main/java/day1/task1/Execute.java
@@ -1,5 +1,8 @@
 package day1.task1;
 
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+
 public class Execute {
     public static void main(String[] args) {
 
@@ -8,5 +11,17 @@ public class Execute {
                 133831, 92793, 148917, 75262, 129853, 60513, 89914, 79584, 64229, 124145, 127684, 142628, 52734, 130649, 87191, 126500, 137058, 109782, 108641, 102147, 132881, 119065,
                 58999, 62462, 105232, 79743, 127994, 143392, 61072, 59375, 57361, 128021, 101544, 135661, 135469, 51693, 103286, 146654, 97886, 133910, 71306, 147224, 73771, 91292, 116892,
                 116906, 107424, 68283, 100285, 105709, 120370, 92931, 146706, 131745, 101710, 85089, 98788, 116232};
+
+        IntUnaryOperator divide = iuo -> iuo / 3;
+        IntUnaryOperator subtract = s -> s - 2;
+        IntBinaryOperator sumTwo = (i1, i2) -> i1 + i2;
+
+        System.out.println("After division by 3: " + divide.applyAsInt(68884));
+
+        System.out.println("Division and subtraction: "
+                + divide.andThen(subtract).applyAsInt(1969)
+                + divide.andThen(subtract).applyAsInt(100756));
+
+        System.out.println("Sum two values: " + sumTwo.applyAsInt(645, 33583));
     }
 }
